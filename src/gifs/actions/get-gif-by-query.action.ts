48ae4db3545038008e8ff2a603088cc2f9ca202a
MiltonCoronado <miltonext@hotmail.com>
2026-04-12 16:53:05 -0500

    feat: (API)implement getGifsByQuery service with mapper

---
+++ src/gifs/actions/get-gif-by-query.action.ts
@@ -0,0 +1,26 @@
+import { giphyApi } from '../api/giphy.api'; //Es la instacia de axios.
+
+import type { GiphyResponse } from '../interfaces/giphy.response';
+import type { Gif } from '../interfaces/gif.interface';
+
+const getGifsByQuery = async (query: string): Promise<Gif[]> => {
+  //Axios ó su Instancia si acepta genericos, no como fetch()!!!
+  const response = await giphyApi<GiphyResponse>('/search', {
+    //object configuration.
+    params: {
+      q: query,
+      limit: 10,
+    },
+  });
+
+  //Patron mapper (mapear la data): tranformar una data que luce de una forma a tranformarla en otra(siendoLaMismasData)
+  return response.data.data.map((gif) => ({
+    id: gif.id,
+    title: gif.title,
+    url: gif.images.original.url,
+    width: Number(gif.images.original.width),
+    height: Number(gif.images.original.height), //con la Fn. constructora Number(), trasforma un string a numero.
+  }));
+};
+
+export { getGifsByQuery };
