249c9f03cac3efde006537c679f4357482ae5211
MiltonCoronado <miltonext@hotmail.com>
2026-04-22 17:39:35 -0500

    refactor: extract search logic to custom hook

--- src/GifApp.tsx
+++ src/GifApp.tsx
@@ -1,32 +1,12 @@
-import { useState } from 'react';
 import { CustomHeader } from './shared/components/CustomHeader';
 import { SearchBar } from './shared/components/SearchBar';
 import { PreviousSearches } from './gifs/components/PreviousSearches';
 import { GifList } from './gifs/components/GifList';
-import type { Gif } from '../src/gifs/interfaces/gif.interface';
-
-import { getGifsByQuery } from './gifs/actions/get-gif-by-query.action';
+import { useGifs } from './gifs/hooks/useGifs';
 
 const GifApp = () => {
-  const [previousTerms, setPreviousTerms] = useState<string[]>([]); //useState() si acepta genericos!!!
-  const [gifslList, setGifsList] = useState<Gif[]>([]);
-
-  const handleTermClicked = (term: string) => {
-    console.log({ term });
-  };
-
-  const handleSearch = async (query: string = '') => {
-    console.log({ query });
-    const newQuery = query.toLowerCase().trim();
-    if (newQuery.length === 0) return;
-    if (previousTerms.includes(newQuery)) return;
-
-    setPreviousTerms([newQuery, ...previousTerms].slice(0, 8)); //(spread operator): crea una nueva copia del array para que React detecte el cambio de referencia y actualice la interfaz.
-
-    const gifs = await getGifsByQuery(query);
-    console.log({ gifs });
-    setGifsList(gifs);
-  };
+  const { gifslList, previousTerms, handleSearch, handleTermClicked } =
+    useGifs();
 
   return (
     <>
